cd272f5cd99ca49cda94af5a9adcd2080861b4b4
oteniyatobi <oluwatobijeremiahoteniya@gmail.com>
2026-02-21 08:43:47 -0800

    .

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[52:77] + " with " + str[:6])
+print(str[52:77] + str[0:6])
