fa851bb79e75a84137bb89fa665089eb86369078
oteniyatobi <oluwatobijeremiahoteniya@gmail.com>
2026-03-18 06:36:12 -0700

    fixed all errors

--- python-more_classes/5-rectangle.py
+++ python-more_classes/5-rectangle.py
@@ -1,3 +1,64 @@
 #!/usr/bin/python3
-def __del__(self):
+"""Module that defines a Rectangle class"""
+
+
+class Rectangle:
+    """Defines a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Initialize rectangle"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Retrieve width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieve height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Return area"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Return perimeter"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """String representation"""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+
+        return "\n".join("#" * self.__width for _ in range(self.__height))
+
+    def __repr__(self):
+        """Recreate instance"""
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        """Called when instance is deleted"""
         print("Bye rectangle...")
