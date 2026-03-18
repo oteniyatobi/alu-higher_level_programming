6b105eaa27f684efa12318ea5a271ad8c5dd5d62
oteniyatobi <oluwatobijeremiahoteniya@gmail.com>
2026-03-18 06:27:02 -0700

    first commit of the day

---
+++ python-more_classes/4-rectangle.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+    def __repr__(self):
+     return "Rectangle({}, {})".format(self.__width, self.__height)
---
+++ python-more_classes/5-rectangle.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+def __del__(self):
+        print("Bye rectangle...")
