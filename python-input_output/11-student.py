2e768ba82cdc63272dc4467dc4354d217df2d9b0
oteniyatobi <oluwatobijeremiahoteniya@gmail.com>
2026-03-25 19:33:10 -0700

    fixed major errors

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,4 +1,27 @@
-def reload_from_json(self, json):
-    """Replace attributes from dictionary"""
-    for key, value in json.items():
-        setattr(self, key, value)
+#!/usr/bin/python3
+"""Student class"""
+
+
+class Student:
+    """Defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return dictionary representation"""
+        if isinstance(attrs, list):
+            new_dict = {}
+            for key in attrs:
+                if hasattr(self, key):
+                    new_dict[key] = getattr(self, key)
+            return new_dict
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace all attributes using dictionary"""
+        for key, value in json.items():
+            setattr(self, key, value)
