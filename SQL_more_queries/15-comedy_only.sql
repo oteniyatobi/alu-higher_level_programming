0baebe1bf157b1469e94dbeaa24b784bea293210
oteniyatobi <oluwatobijeremiahoteniya@gmail.com>
2026-03-27 19:08:20 -0700

    done

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,1 +1,7 @@
-15-comedy_only.sql
+-- lists all comedy shows
+SELECT tv_shows.title
+FROM tv_shows, tv_show_genres, tv_genres
+WHERE tv_shows.id = tv_show_genres.show_id
+AND tv_show_genres.genre_id = tv_genres.id
+AND tv_genres.name = "Comedy"
+ORDER BY tv_shows.title;
