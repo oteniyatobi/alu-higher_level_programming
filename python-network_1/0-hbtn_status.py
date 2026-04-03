5fe22f36ec7cc514a80740b2754a95cea8509e32
oteniyatobi <oluwatobijeremiahoteniya@gmail.com>
2026-04-03 16:23:21 +0200

    Fix URL to intranet.hbtn.io in status scripts

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -3,7 +3,7 @@
 import urllib.request
 
 if __name__ == "__main__":
-    url = 'https://alu-intranet.hbtn.io/status'
+    url = 'https://intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as r:
         body = r.read()
     print("Body response:")
