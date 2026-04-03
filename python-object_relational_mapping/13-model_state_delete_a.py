dab333b0fb26542283593e8e4e26cd8cbb3eef37
oteniyatobi <oluwatobijeremiahoteniya@gmail.com>
2026-04-03 17:51:49 +0200

    Use bulk delete for states containing 'a'

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -14,8 +14,8 @@ if __name__ == "__main__":
     )
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).filter(State.name.like('%a%')).all()
-    for state in states:
-        session.delete(state)
+    session.query(State).filter(
+        State.name.like('%a%')
+    ).delete(synchronize_session=False)
     session.commit()
     session.close()
